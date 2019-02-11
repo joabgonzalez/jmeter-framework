95164900a47fc905ea8eb6ec2f3408289a6a1f96
Joab Gonzalez <joab.gonzalez@tsoftlatam.com>
2019-02-11 16:18:32 -0300

    agregando visualizer

--- ansible/roles/scripts-utils-manager/files/runTest.sh
+++ ansible/roles/scripts-utils-manager/files/runTest.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+read IP < /home/linux/performance/utils/ipAddressSlaves.csv
 docker exec -it $(docker ps -f name=jmeter-stack_master* -q) \
-sh /performance/apache-jmeter/bin/run.sh $1 $2
+sh /performance/apache-jmeter/bin/run.sh $1 $IP
