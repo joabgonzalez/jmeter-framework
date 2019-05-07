4e0c6b3c919573b7c6b3aba6d0cac1e640a3d924
Joab Gonzalez <joab.gonzalez@tsoftglobal.com>
2019-05-07 16:58:49 -0400

    ajuste de tiempos

--- ansible/roles/scripts-utils-manager/files/regenerateMaster.sh
+++ ansible/roles/scripts-utils-manager/files/regenerateMaster.sh
@@ -3,5 +3,5 @@ date=`date "+%Y%m%d%H%M%S"`
 docker exec -it $(docker ps -f name=jmeter-stack_master* -q) \
 cat /performance/apache-jmeter/bin/results/test-result.jtl > $date.jtl
 docker service scale jmeter-stack_master=0
-sleep 1
+sleep 5s
 docker service scale jmeter-stack_master=1
--- ansible/roles/scripts-utils-manager/files/regenerateSlaves.sh
+++ ansible/roles/scripts-utils-manager/files/regenerateSlaves.sh
@@ -1,5 +1,5 @@
 
 #!/bin/sh
 docker service scale jmeter-stack_slave=0
-sleep 1
+sleep 5s
 docker service scale jmeter-stack_slave=$1
